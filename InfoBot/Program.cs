1413dd3a27eee2b157b94cac35298f92a32dbcb5
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-09-15 11:33:04 +0200

    Base starting program

---
+++ InfoBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoBot
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static async Task AsyncMain(string[] args)
+        {
+            string Token;
+            Console.Write("Token :");
+            Token = Console.ReadLine();
+        }
+
+        private static void Main(string[] args)
+        {
+            Task.Run(() => AsyncMain(args)).GetAwaiter().GetResult();
+        }
+
+        #endregion Private Methods
+    }
+}
