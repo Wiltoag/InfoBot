11dbac5210f6d77ea83418caa09365d134c25d28
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-09-15 13:03:31 +0200

    added input parsing and connection

---
+++ InfoBot/Dispatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace InfoBot
+{
+    internal class Dispatcher
+    {
+        #region Public Constructors
+
+        public Dispatcher()
+        {
+            Queue = new Queue<Action>();
+        }
+
+        #endregion Public Constructors
+
+        #region Private Properties
+
+        private Queue<Action> Queue { get; set; }
+
+        #endregion Private Properties
+
+        #region Public Methods
+
+        /// <summary>
+        /// Execute on the main loop the command.
+        /// </summary>
+        /// <param name="action">Command to execute</param>
+        public void Execute(Action action) => Queue.Enqueue(action);
+
+        public Action GetNext() => Queue.Count > 0 ? Queue.Dequeue() : null;
+
+        #endregion Public Methods
+    }
+}
--- InfoBot/Program.cs
+++ InfoBot/Program.cs
@@ -2,24 +2,114 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using DSharpPlus;
 
 namespace InfoBot
 {
     internal class Program
     {
+        #region Private Properties
+
+        private static DiscordClient Discord { get; set; }
+        private static Dispatcher Dispatcher { get; set; }
+
+        #endregion Private Properties
+
         #region Private Methods
 
         private static async Task AsyncMain(string[] args)
         {
-            string Token;
+            string token;
             Console.Write("Token :");
-            Token = Console.ReadLine();
+            token = Console.ReadLine();
+            Discord = new DiscordClient(new DiscordConfiguration() { Token = token, TokenType = TokenType.Bot });
+            Console.WriteLine("Connecting...");
+            bool error = false;
+            try
+            {
+                if (!Discord.ConnectAsync().Wait(TimeSpan.FromSeconds(10)))
+                    throw new Exception("Unable to connect (10 sec timeout)");
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fatal error : " + e.ToString());
+                error = true;
+                Console.ReadKey();
+            }
+            if (error)
+                Environment.Exit(0);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Connected");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Dispatcher = new Dispatcher();
+            var consoleThread = new Thread(ConsoleManager);
+            consoleThread.Start();
+
+            while (true)
+            {
+                Dispatcher.GetNext()?.Invoke();
+                Thread.Sleep(TimeSpan.FromMilliseconds(50));
+            }
+        }
+
+        private static void ConsoleManager()
+        {
+            while (true)
+            {
+                Console.Write('>');
+                var input = Console.ReadLine();
+                ParseInput(input, out string command, out string[] args);
+            }
         }
 
         private static void Main(string[] args)
         {
             Task.Run(() => AsyncMain(args)).GetAwaiter().GetResult();
+        }
+
+        private static void ParseInput(string input, out string command, out string[] args)
+        {
+            command = "";
+            var listArgs = new List<string>();
+            int index = 0;
+            while (index < input.Length)
+            {
+                var currChar = input[index];
+                if (currChar == ' ')
+                    break;
+                else
+                    command += currChar;
+                index++;
+            }
+            index++;
+            var currArg = "";
+            bool ignoreSpaces = false;
+            while (index < input.Length)
+            {
+                var currChar = input[index];
+                if (currChar == '\\' && input.Length > index + 1)
+                {
+                    currChar = input[++index];
+                    currArg += currChar;
+                }
+                else if (currChar == ' ' && !ignoreSpaces)
+                {
+                    if (currArg.Length > 0)
+                        listArgs.Add(currArg);
+                    currArg = "";
+                }
+                else if (currChar == '"')
+                    ignoreSpaces = !ignoreSpaces;
+                else
+                    currArg += currChar;
+                index++;
+            }
+            if (currArg.Length > 0)
+                listArgs.Add(currArg);
+            args = listArgs.ToArray();
         }
 
         #endregion Private Methods
