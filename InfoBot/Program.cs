cbdee8c8f47b30be7887a434451893ee0cfa2000
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-09-29 18:27:09 +0200

    added a similarity function to recognize strings

--- InfoBot/Program.cs
+++ InfoBot/Program.cs
@@ -19,19 +19,31 @@ namespace InfoBot
         #region Private Fields
 
         private const bool DEBUG = false;
+
         private static List<Autorun> Autoruns;
+
         private static WebClient Client;
+
         private static ConsoleColor DefaultColor;
+
         private static DiscordGuild DUTInfoServer;
+
         private static DiscordChannel[] EdtChannel;
+
         private static List<EdtDayMessage>[] EdTMessages;
-        private static DateTime LastSaveDate;
+
         private static int[] OldICalHash;
+
         private static int RevengeCurrLine;
+
         private static string[] revengeLines;
+
         private static List<SavedPoll> SavedPolls;
+
         private static List<SavedVote> SavedVotes;
+
         private static DiscordGuild TestServer;
+
         private static DiscordRole[] TPRoles;
 
         #endregion Private Fields
@@ -175,7 +187,7 @@ namespace InfoBot
                     ExecuteAsyncMethod(async () =>
                     {
                         var content = GetSimplifiedString(arg.Message.Content);
-                        if (content.Contains(GetSimplifiedString(revengeLines[RevengeCurrLine])) && !arg.Author.IsBot)
+                        if ((content.Contains(GetSimplifiedString(revengeLines[RevengeCurrLine])) || CalculateSimilarity(content, revengeLines[RevengeCurrLine]) >= .75f) && !arg.Author.IsBot && RevengeCurrLine > 0)
                         {
                             RevengeCurrLine += 2;
                             if (RevengeCurrLine - 1 < revengeLines.Length)
@@ -223,6 +235,54 @@ namespace InfoBot
             }
         }
 
+        private static double CalculateSimilarity(string source, string target)
+        {
+            //https://social.technet.microsoft.com/wiki/contents/articles/26805.c-calculating-percentage-similarity-of-2-strings.aspx
+            if ((source == null) || (target == null)) return 0.0;
+            if ((source.Length == 0) || (target.Length == 0)) return 0.0;
+            if (source == target) return 1.0;
+
+            int stepsToSame = ComputeLevenshteinDistance(source, target);
+            return (1.0 - ((double)stepsToSame / Math.Max(source.Length, target.Length)));
+        }
+
+        private static int ComputeLevenshteinDistance(string source, string target)
+        {
+            if ((source == null) || (target == null)) return 0;
+            if ((source.Length == 0) || (target.Length == 0)) return 0;
+            if (source == target) return source.Length;
+
+            int sourceWordCount = source.Length;
+            int targetWordCount = target.Length;
+
+            // Step 1
+            if (sourceWordCount == 0)
+                return targetWordCount;
+
+            if (targetWordCount == 0)
+                return sourceWordCount;
+
+            int[,] distance = new int[sourceWordCount + 1, targetWordCount + 1];
+
+            // Step 2
+            for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++) ;
+            for (int j = 0; j <= targetWordCount; distance[0, j] = j++) ;
+
+            for (int i = 1; i <= sourceWordCount; i++)
+            {
+                for (int j = 1; j <= targetWordCount; j++)
+                {
+                    // Step 3
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    // Step 4
+                    distance[i, j] = Math.Min(Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1), distance[i - 1, j - 1] + cost);
+                }
+            }
+
+            return distance[sourceWordCount, targetWordCount];
+        }
+
         private static void ConsoleManager()
         {
             while (true)
@@ -268,7 +328,7 @@ namespace InfoBot
                             break;
                     }
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
                 }
             }
