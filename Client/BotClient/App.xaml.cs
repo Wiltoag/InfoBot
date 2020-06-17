af57315ecdf916d46a8c625271db7eaa5d36d474
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-06-17 09:44:27 +0200

    finished client interface

--- Client/BotClient/App.xaml.cs
+++ Client/BotClient/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace BotClient
 {
